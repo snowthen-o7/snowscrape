379b055a6e72de4a1f662fce724c7ab9db7447c7
Alex Diaz <alexitofrancis@gmail.com>
2024-09-12 11:21:56 -0700

    Version corrections

---
+++ app/(application)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="p-8 text-white">
+      <h1 className="text-4xl mb-6">Dashboard</h1>
+      {/* Rest of your code */}
+    </div>
+  );
+}
